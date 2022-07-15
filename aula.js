6bcfefb132bf0abfa5d1194c70058ca44191db81
Aston8522 <gabriel.aston25@gmail.com>
2022-07-15 20:19:14 -0300

    test aula

--- aula.js
+++ aula.js
@@ -1,10 +1,16 @@
 // let idade = 5;
 // console.log(idade)
 // camelCase
-let nome ="aston";
-console.log(nome)
-let altura = 1.72;
-console.log(altura);
+// let nome ="aston";
+// console.log(nome)
+// let altura = 1.72;
+// console.log(altura);
 
-let comprimento = "85cm";
-console.log(comprimento);
+// let comprimento = "85cm";
+// console.log(comprimento);
+
+let  nome = 'gabriel';
+let idade= 21;
+let estaAprovado = false;
+let  sobrenome ; undefined
+let corSelecionada = null; 
